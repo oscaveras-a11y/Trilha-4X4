d5a26245897e971401d27862c59d74f740580708
oscaveras-a11y <oscaveras@gmail.com>
2026-09-21 13:52:58 -0300

    chore: cache mobile PWA assets

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'trilha-4x4-v28';
+const CACHE_NAME = 'trilha-4x4-v29';
 const APP_SHELL = [
   '/',
   '/index.html',
@@ -8,6 +8,9 @@ const APP_SHELL = [
   '/trilha.html',
   '/manifest.webmanifest',
   '/icon.svg',
+  '/apple-touch-icon.png',
+  '/icon-192.png',
+  '/icon-512.png',
 ];
 
 self.addEventListener('install', (event) => {
